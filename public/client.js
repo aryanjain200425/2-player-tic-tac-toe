48f430f6b3c104eac2a6d73f79904cc0366581f7
aryanjain200425 <aryanrashijain@gmail.com>
2024-05-26 14:10:45 -0400

    displaying the symbol to the player

--- public/client.js
+++ public/client.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const message = document.getElementById('message');
 
     const socket = io();
 
-    socket.on('disconnect')
+    socket.on('player-joined', (symbol) =>{
+        message.innerHTML = `Your symbol is ${symbol}`;
+    });
+
 
 });
--- server.js
+++ server.js
@@ -12,9 +12,26 @@ app.use(express.static('public'));
 
 server.listen(port, () => console.log(`Server is running on port ${port}`));
 
+let players = new Array();
 
 io.on('connection', (socket) => {
+
+    // when a player joins
     console.log('A user connected:', socket.id);
+
+    players.push(socket.id)
+
+    if(players.length === 1){
+        socket.emit('player-joined', 'X');
+    }
+    else if(players.length == 2){
+        socket.emit('player-joined', 'O');
+    }
+    else{
+        socket.emit('player-joined', 'Spectating');
+    }
+    
+
 
     socket.on('disconnect', () =>{
         console.log('A user disconnected: ', socket.id);
